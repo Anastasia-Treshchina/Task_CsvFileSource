7e9d87e6f191ac1082339bca4bf9b492ea824971
Anastasia Treshchina <anastasiya.treschina@gmail.com>
2022-02-16 22:52:01 +0400

    Add files

---
+++ src/test/java/BonusServiceTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BonusServiceTest {
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data.csv")
+    void shouldCalculateRegisteredAndUnderLimit(String testName, long amount,
+                                                boolean registered, long expected) {
+        BonusService service = new BonusService();
+        long actual = service.calculate(amount, registered);
+        assertEquals(expected, actual);
+    }
+}
